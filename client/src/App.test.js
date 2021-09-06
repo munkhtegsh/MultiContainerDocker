0bb550e728e06c224e6c82ba6c0ca8b3b54b17da
Munkhtegsh <munkhtegsh@outlook.com>
2021-09-06 11:02:29 -0700

    Configure multi container with docker

---
+++ client/src/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+import { isTypedArray } from 'util/types';
+
+it('renders without crashing', () => {});
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
